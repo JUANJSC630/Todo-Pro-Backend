a7fad2ca7764e7f7effe87f50467c349121a3e6f
Juan Saldarriaga <juan@ocmiwc.com>
2025-04-04 08:45:53 -0500

    refactor: update server port configuration to use environment variable or default to 3000

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 
 bootstrap();
